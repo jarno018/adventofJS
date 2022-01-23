43af0e730cc80826e2a9a3b1a083aa54da1328d3
jarno018 <jarno.lannoo@hotmail.com>
2022-01-23 23:56:29 +0100

    Update index.svelte

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Advent of JavaScript</h1>
+<p>Visit <a href="https://adventofjs.com">adventofjs</a> to see all challenges</p>
